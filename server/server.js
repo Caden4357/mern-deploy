045cc352b21c551feeda2d0fd3769b22b411899b
Caden Wilcox <wilcox4357@gmail.com>
2022-12-29 14:34:31 -0800

    test

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 
 app.use(cookieParser())
 app.use(cors({credentials:true, origin:'http://localhost:3000'}))
-require('./routes/albumRoutes')(app)
 require('./routes/userRoutes')(app)
+require('./routes/albumRoutes')(app)
     
 app.listen(8000, () => console.log("The server is all fired up on port 8000"));
